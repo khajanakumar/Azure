f82b981a14444a925cb67227a6f78a71af67f53d
khajanakumar <52144367+khajanakumar@users.noreply.github.com>
2024-02-29 15:49:03 -0600

    Update 3_query_all_files_from_selected_subfolder.sql

--- openrowset/sql/3_query_all_files_from_selected_subfolder.sql
+++ openrowset/sql/3_query_all_files_from_selected_subfolder.sql
@@ -4,8 +4,8 @@ SELECT
     distinct result.filename() as file_name
 FROM
     OPENROWSET(
-        BULK ( 'https://dp203synapsecoursedl.dfs.core.windows.net/dp203synapseprimarycontainer/trip_data_green_csv/year=2021/month=01/*',
-                'https://dp203synapsecoursedl.dfs.core.windows.net/dp203synapseprimarycontainer/trip_data_green_csv/year=2021/month=02/*'
+        BULK ( 'https://<storage Account name>.dfs.core.windows.net/dp203synapseprimarycontainer/trip_data_green_csv/year=2021/month=01/*',
+                'https://<storage Account name>.dfs.core.windows.net/dp203synapseprimarycontainer/trip_data_green_csv/year=2021/month=02/*'
             ),
         FORMAT = 'CSV',
         PARSER_VERSION = '2.0',
